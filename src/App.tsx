01936311fe2cbe12849e1ce8183774f9be978bde
usmain-dot <usmaingumaa08@gmail.com>
2025-12-19 15:05:16 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -576,7 +576,7 @@ if (authLoading || servicesLoading) {
             <img
               src="/bridge-logo.png"
                alt="Bridge Logo"
-              className="h-10 w-auto"
+              className="h-6 w-auto"
              />
             </div>
 
